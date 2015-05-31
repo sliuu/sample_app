99b0be7dac4d0df4e7049b9e553e21fe646a9173
Stephanie Liu <sl14@williams.edu>
2015-05-31 12:08:34 +0000

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,6 +11,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
   end
+  
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name: "Example User", 
+                                            email: "user@example.com", 
+                                            password: "password", 
+                                            password_confirmation: "password" }
+    end
+    assert_template 'users/show'
+  end
+                                            
 end
 
 #assert_no_difference checks user count before and after "do", and
